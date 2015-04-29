b9158c654331167b20ed0e31fecdca53229797d7
Ricky Zhang <rickyzhang@rickyznang.me>
2015-04-29 09:46:58 +0800

    add

--- Triangle/solution.c
+++ Triangle/solution.c
@@ -1,22 +1,22 @@
 int minimumTotal(int **triangle, int numRows) {
-    int i;
-    int *result=(int *)malloc(numRows*sizeof(int));
+	int i;
+	int *result=(int *)malloc(numRows*sizeof(int));
 	int min;
-    for(i=0;i<numRows;i++)
-    {
-        result[i]=triangle[numRows-1][i];
-    }
-    for(i=numRows-2;i>=0;i--)
-    {
-        int j;
-        for(j=0;j<=i;j++)
-        {
-            int p=triangle[i][j]+result[j];
-            int k=triangle[i][j]+result[j+1];
-            result[j]=p<k?p:k;
-        }
-    }
-    min=result[0];
+	for(i=0;i<numRows;i++)
+	{
+		result[i]=triangle[numRows-1][i];
+	}
+	for(i=numRows-2;i>=0;i--)
+	{
+		int j;
+		for(j=0;j<=i;j++)
+		{
+			int p=triangle[i][j]+result[j];
+			int k=triangle[i][j]+result[j+1];
+			result[j]=p<k?p:k;
+		}
+	}
+	min=result[0];
 	free(result);
 	return min;
 }
